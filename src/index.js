992d35072d408b55143cae2592b02e7e881e1c37
Mmanawa Thato Marongwe <mashalammanawa@gmail.com>
2023-09-15 14:28:07 +0200

    Updated index,js to get current temperature, description for Johannesburg

---
+++ src/index.js
@@ -0,0 +1,15 @@
+function displayJohannesburg(response){
+    console.log(response.data.weather); 
+    let cityElement = document.querySelector("#city");
+    cityElement.innerHTML = response.data.city;
+    let temperatureElement = document.querySelector("#temperature");
+    temperatureElement.innerHTML = Math.round(response.data.temperature.current);
+    let descriptionElement = document.querySelector("#description");
+    descriptionElement.innerHTML = response.data.condition.description;
+
+}
+
+let apiKey = "e6ba34ft27a3a8ccco506b17217f3b8b";
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query={Johannesburg}&key=${apiKey}`;
+
+axios.get(apiUrl).then(displayJohannesburg);
